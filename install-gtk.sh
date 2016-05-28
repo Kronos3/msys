8c2c4411461f1017dc5a4f3aa14a509b2895d03e
Andrei Tumbar <dovakhiin1359@gmail.com>
2016-05-28 19:04:14 -0400

    Copy install script from html parser

--- install-gtk.sh
+++ install-gtk.sh
@@ -1,41 +1,50 @@
-#*
-# * install-gtk.sh
-# * 
-# * Copyright 2016 ATADMIN <ATADMIN@KRONOS>
-# * 
-# * This program is free software; you can redistribute it and/or modify
-# * it under the terms of the GNU General Public License as published by
-# * the Free Software Foundation; either version 2 of the License, or
-# * (at your option) any later version.
-# * 
-# * This program is distributed in the hope that it will be useful,
-# * but WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# * GNU General Public License for more details.
-# * 
-# * You should have received a copy of the GNU General Public License
-# * along with this program; if not, write to the Free Software
-# * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# * MA 02110-1301, USA.
-# * 
-# * 
+:
+@echo off
 
-echo "Updating package tree..."
-pacman -Sy pacman
-pacman -Syu
+rem ember value of GOTO: is used to know recursion has happened.
+if "%1" == "GOTO:" goto %2
 
-echo "Updateing System"
-pacman -Su
+if NOT "x%WD%" == "x" set WD=
 
-echo "Installing GTK"
-pacman -S python3
-pacman -S python2
-pacman -S mingw32/mingw-w64-x86_64-gtksourceview3
-pacman -S mingw64/mingw-w64-x86_64-clutter-gtk
-pacman -S mingw64/mingw-w64-x86_64-gtk3
-pacman -S gcc
-pacman -S mingw-w64-x86_64-gobject-introspection
-pacman -S mingw-w64-x86_64-gobject-introspection-runtime
-pacman -S mingw64/mingw-w64-x86_64-python2-gobject 
-pacman -S mingw64/mingw-w64-x86_64-python3-gobject
-pacman -S mingw32/mingw-w64-x86_64-gtksourceview3
+rem ember command.com only uses the first eight characters of the label.
+goto _WindowsNT
+
+start /min %COMSPEC% /e:4096 /c %0 GOTO: _Resume %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto EOF
+
+rem ember that we execute here if we recursed.
+:_Resume
+for %%F in (1 2 3) do shift
+if NOT EXIST %WD%msys-2.0.dll set WD=.\usr\bin\
+
+rem ember that we get here even in command.com.
+:_WindowsNT
+
+if NOT EXIST %WD%msys-2.0.dll set WD=%~dp0usr\bin\
+
+set MSYSTEM=MINGW64
+rem To activate windows native symlinks uncomment next line
+rem set MSYS=winsymlinks:nativestrict
+rem Set debugging program for errors
+rem set MSYS=error_start:%WD%../../mingw64/bin/qtcreator.exe^|-debug^|^<process-id^>
+set MSYSCON=mintty.exe
+if "x%1" == "x-consolez" set MSYSCON=console.exe
+if "x%1" == "x-mintty" set MSYSCON=mintty.exe
+
+if "x%MSYSCON%" == "xmintty.exe" goto startmintty
+if "x%MSYSCON%" == "xconsole.exe" goto startconsolez
+
+:startmintty
+if NOT EXIST %WD%mintty.exe goto startsh
+start %WD%mintty -i /msys2.ico /usr/bin/sh install-gtk.sh
+exit
+
+:startconsolez
+cd %WD%..\lib\ConsoleZ
+sleep
+exit
+
+:startsh
+exit
+
+:EOF
