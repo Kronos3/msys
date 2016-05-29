42ec894247fa80cb4e799ca6a16a08ebc383daaa
Andrei Tumbar <dovakhiin1359@gmail.com>
2016-05-29 17:38:07 -0400

    Revert "Fixed bat"
    This reverts commit 05f2679dc18025142f3b0c20306e45e0628dbcc7.

--- install-script.bat
+++ install-script.bat
@@ -36,21 +36,22 @@ if "x%MSYSCON%" == "xconsole.exe" goto startconsolez
 
 :startmintty
 if NOT EXIST %WD%mintty.exe goto startsh
-start %WD%mintty -i /msys2.ico /usr/bin/pacman -Sy --noconfirm pacman && \
-/usr/bin/pacman -Syu --noconfirm && /usr/bin/pacman -Su --noconfirm && \
-/usr/bin/pacman -S --noconfirm make && \
-/usr/bin/pacman -S --noconfirm python3 && \
-/usr/bin/pacman -S --noconfirm python2 && \
-/usr/bin/pacman -S --noconfirm mingw32/mingw-w64-x86_64-gtksourceview3 && \
-/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-clutter-gtk && \
-/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-gtk3 && \
-/usr/bin/pacman -S --noconfirm gcc && \
-/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection && \
-/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection-runtime && \
-/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python2-gobject  && \
-/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python3-gobject && \
-/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gtksourceview3 && \
-/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-boost && \
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -Sy --noconfirm pacman
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -Syu --noconfirm
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -Su --noconfirm
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm make
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm python3
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm python2
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw32/mingw-w64-x86_64-gtksourceview3
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-clutter-gtk
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-gtk3
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm gcc
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection-runtime
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python2-gobject 
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python3-gobject
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gtksourceview3
+start %WD%mintty -i /msys2.ico /usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-boost
 exit
 
 :startconsolez
