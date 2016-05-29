84c61d501d9183cf3caeb6f503fce1553a14a530
Andrei Tumbar <dovakhiin1359@gmail.com>
2016-05-29 17:38:35 -0400

    Revert "Made some changes to fix installer"
    This reverts commit 3a26f385484040837e20c83be48ecc88f1fc5ebd.

---
+++ gtk-script.sh
@@ -0,0 +1,16 @@
+/usr/bin/pacman -Sy --noconfirm pacman
+/usr/bin/pacman -Syu --noconfirm
+/usr/bin/pacman -Su --noconfirm
+/usr/bin/pacman -S --noconfirm make
+/usr/bin/pacman -S --noconfirm python3
+/usr/bin/pacman -S --noconfirm python2
+/usr/bin/pacman -S --noconfirm mingw32/mingw-w64-x86_64-gtksourceview3
+/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-clutter-gtk
+/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-gtk3
+/usr/bin/pacman -S --noconfirm gcc
+/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection
+/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gobject-introspection-runtime
+/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python2-gobject 
+/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-python3-gobject
+/usr/bin/pacman -S --noconfirm mingw-w64-x86_64-gtksourceview3
+/usr/bin/pacman -S --noconfirm mingw64/mingw-w64-x86_64-boost
